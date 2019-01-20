89f25e2fd639c3b085e6be3ee236761693e205f5
HelioStrike <rageofepicfury@gmail.com>
2019-01-20 11:15:45 +0530

    committed

--- putInterviewHistory.php
+++ putInterviewHistory.php
@@ -1,5 +1,12 @@
-<?php include 'connect.php'; ?>
 <?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "interview_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 $command = "INSERT INTO interview_history(interview_id, question_id, student_id, question, correct_answer, response_answer, grade) VALUES("
         .$_GET["interview_id"].",".$_GET["question_id"].",".$_GET["student_id"].",".$_GET["question"].",".$_GET["correct_answer"]
